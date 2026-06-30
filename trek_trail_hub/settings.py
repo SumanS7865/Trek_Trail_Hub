ec617e83aa736ffbed276a4c80274da895224171
ashok_bhusal <susa_bim2076@lict.edu.np>
2026-06-30 22:00:50 +0545

    Fixed static route in vercel.json

--- trek_trail_hub/settings.py
+++ trek_trail_hub/settings.py
@@ -119,8 +119,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = "static/"
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_URL = "/static/"
+STATICFILES_DIRS = [BASE_DIR / "static"]
 
 # 👇 यो नयाँ कुरा थप्ने (यसले बाहिरको static फोल्डर चिनाउँछ) 👇
 STATICFILES_DIRS = [
